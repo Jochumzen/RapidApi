3102c3e48186c12559e0f5852ee167195541019b
JENO87 <jens.c.norell@gmail.com>
2022-06-28 10:33:12 +0200

    fiddling with reverse geocode

--- datasource_trueway/src/main/java/com/mapifesto/datasource_trueway/Mapper.kt
+++ datasource_trueway/src/main/java/com/mapifesto/datasource_trueway/Mapper.kt
@@ -1,30 +1,62 @@
 package com.mapifesto.datasource_trueway
 
 import com.mapifesto.domain.RapidReverseItem
-import com.mapifesto.domain.RapidReverseItems
 
 object Mapper {
 
-    fun createRapidReverseItem(dto: RapidDto): RapidReverseItems? {
+    fun createRapidReverseItem(dto: RapidDto): RapidReverseItem {
 
-        return RapidReverseItems(
-            items = dto.results.map {
-                RapidReverseItem(
-                    address = it.address,
-                    postalCode = it.postalCode,
-                    country = it.country,
-                    region = it.region,
-                    area = it.area,
-                    locality = it.locality,
-                    sublocality = it.sublocality,
-                    street = it.street,
-                    house = it.house,
-                    lat = it.locationDto.lat.toString(),
-                    lon = it.locationDto.lon.toString(),
-                    locationType = it.locationType,
-                    type = it.type,
-                )
-            }
-        )
+        var address = ""
+        var postalCode = ""
+        var country = ""
+        var region = ""
+        var area = ""
+        var locality = ""
+        var sublocality = ""
+        var street = ""
+        var house = ""
+        var lat = ""
+        var lon = ""
+        var locationType = ""
+        var type = ""
+
+        dto.results.find {
+            it.type == "street_address"
+        }.apply {
+            postalCode = this?.postalCode ?: ""
+            country = this?.country ?: ""
+            region = this?.region ?: ""
+            street = this?.street ?: ""
+            sublocality = this?.sublocality ?: ""
+
+        }
+
+        dto.results.find {
+            it.type == "area"
+        }.apply {
+            area = this?.area ?: ""
+        }
+
+        dto.results.find {
+            it.type == "locality"
+        }.apply {
+            locality = this?.locality ?: ""
+        }
+
+        return RapidReverseItem(
+                address = address,
+                postalCode = postalCode,
+                country = country,
+                region = region,
+                area = area,
+                locality = locality,
+                sublocality = sublocality,
+                street = street,
+                house = house,
+                lat = lat,
+                lon = lon,
+                locationType = locationType,
+                type = type,
+            )
     }
 }
